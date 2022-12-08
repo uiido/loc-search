ee071fe5560a1745d3616e0c8e67075c7312accc
uiido <uiido@yahoo.com>
2022-12-08 13:50:53 -0500

    2nd update

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
-$(".btn-primary").click(function () {
-    var input = $(this).val();
-    console.log(input);
+$(".btn-primary").click(function (event) {
+    event.preventDefault();
+    var input = $("#textInput").val();
+    var format = $("#format").val();
+    'https://www.loc.gov/' + format + '/?q=' + input + '&fo=json';
+
 }
 )
-
 // https://www.loc.gov/search/?q=baseball&fo=json
+
+// 'https://www.loc.gov/search/?q=' + q + '&fo=json';
